bddee8414442d12f7c14a9c1e8b8ca1f5cad77d8
lapael1 <lapael6@gmail.com>
2024-08-12 16:27:01 +0900

    현대그룹 클론코딩 , 메뉴기능 , nav영억 마우스리브 시 active 비활성화, 언어 펼침 기능, transition 기능

---
+++ js/script.js
@@ -0,0 +1,28 @@
+window.addEventListener("load", function () {
+    //   메뉴기능  //
+    const nav = this.document.querySelector(".nav");
+    const btMenu = this.document.querySelector(".bt-menu");
+    const navClose = this.document.querySelector(".nav-close");
+    btMenu.addEventListener("click", function () {
+      nav.classList.add("nav-active");
+    });
+    navClose.addEventListener("click", () => {
+      nav.classList.remove("nav-active");
+    });
+    //   nav영역에 마우스가 벗어나면 nav-active 비활성화한다.
+    nav.addEventListener("mouseleave", () => {
+      nav.classList.remove("nav-active");
+    });
+    // 언어 펼침 기능
+    const langageLi = this.document.querySelector(".language li");
+    const language = this.document.querySelector(".language");
+    const langBox = this.document.querySelector(".language-box");
+    langBox.addEventListener("click", function () {
+      langBox.classList.toggle("language-box-active");
+    });
+    // langageLi에 transition기능
+    setTimeout(function () {
+      langageLi.style.transition = "all 0.2s";
+    }, 500);
+  });
+  
